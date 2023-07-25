6541dc4acf7b8702812e2cabdd173a581cd2bb75
Mark Sabotta <mark.sabotta@gmail.com>
2023-07-24 21:34:49 -0600

    initializing pokedraft testbuild

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+/*let pokes = ["sprigatito", "floragato", "meowscarada", "fuecoco", "crocalor", "skeledirge", "quaxly", "quaxwell", "quaquaval", "lechonk", "oinkologne", "tarountula", "spidops", "nymble", "lokix", "hoppip", "skiploom", "jumpluff", "fletchling", "fletchinder", "talonflame", "pawmi", "pawmo", "pawmot", "houndour", "houndoom", "yungoos", "gumshoos", "skwovet", "greedent", "sunkern", "sunflora", "kricketot", "kricketune", "scatterbug", "spewpa", "vivillon", "combee", "vespiquen", "rookidee", "corvisquire", "corviknight", "happiny", "chansey", "blissey", "azurill", "marill", "azumarill", "surskit", "masquerain", "buizel", "floatzel", "wooper", "quagsire", "wooper-paldean", "clodsire", "psyduck", "chewtle", "drednaw", "igglybuff", "jigglypuff", "wigglytuff", "ralts", "kirlia", "gardevoir", "gallade", "drowzee", "hypno", "gastly", "haunter", "gengar", "tandemaus", "maushold", "pichu", "pikachu", "raichu", "fidough", "dachsbun", "slakoth", "vigoroth", "slaking", "bounsweet", "steenee", "tsareena", "smoliv", "dolliv", "arboliva", "bonsly", "sudowoodo", "rockruff", "lycanroc", "rolycoly", "carkol", "coalossal", "shinx", "luxio", "luxray", "starly", "staravia", "staraptor", "oricorio", "mareep", "flaaffy", "ampharos", "petilil", "lilligant", "lilligant-hisuian", "shroomish", "breloom", "applin", "flapple", "appletun", "spoink", "grumpig", "squawkabilly", "misdreavus", "mismagius", "makuhita", "hariyama", "crabrawler", "crabominable", "salandit", "salazzle", "phanpy", "donphan", "cufant", "copperajah", "gible", "gabite", "garchomp", "nacli", "naclstack", "garganacl", "wingull", "pelipper", "magikarp", "gyarados", "arrokuda", "barraskewda", "basculin", "basculegion", "gulpin", "swalot", "meowth", "meowth-alolan", "meowth-galarian", "persian", "persian-alolan", "perrserker", "drifloon", "drifblim", "flabebe", "floette", "florges", "diglett", "dugtrio", "torkoal", "numel", "camerupt", "bronzor", "bronzong", "axew", "fraxure", "haxorus", "mankey", "primeape", "annihilape", "meditite", "medicham", "riolu", "lucario", "charcadet", "armarouge", "ceruledge", "barboach", "whiscash", "tadbulb", "bellibolt", "goomy", "sliggoo", "sliggoo-hisuian", "goodra", "goodra-hisuian", "croagunk", "toxicroak", "wattrel", "kilowattrel", "eevee", "vaporeon", "jolteon", "flareon", "espeon", "umbreon", "leafeon", "glaceon", "sylveon", "dunsparce", "dudunsparce", "deerling", "sawsbuck", "girafarig", "farigiraf", "grimer", "grimer-alolan", "muk", "muk-alolan", "maschiff", "mabosstiff", "toxel", "toxtricity", "dedenne", "pachirisu", "shroodle", "grafaiai", "stantler", "wyrdeer", "foongus", "amoonguss", "voltorb", "voltorb-hisuian", "electrode", "electrode-hisuian", "magnemite", "magneton", "magnezone", "ditto", "growlithe", "growlithe-hisuian", "arcanine", "arcanine-hisuian", "teddiursa", "ursaring", "ursaluna", "zangoose", "seviper", "swablu", "altaria", "skiddo", "gogoat", "tauros", "tauros-paldean", "tauros-paldean-fire", "tauros-paldean-water", "litleo", "pyroar", "stunky", "skuntank", "zorua", "zorua-hisuian", "zoroark", "zoroark-hisuian", "sneasel", "sneasel-hisuian", "weavile", "sneasler", "murkrow", "honchkrow", "gothita", "gothitelle", "sinistea", "polteageist", "mimikyu", "klefki", "indeedee", "indeedee-female", "bramblin", "brambleghast", "toedscool", "toedscruel", "tropius", "fomantis", "lurantis", "klawf", "capsakid", "scovillain", "cacnea", "cacturne", "rellor", "rabsca", "venonat", "venomoth", "pineco", "forretress", "scyther", "kleavor", "scizor", "heracross", "flittle", "espathra", "hippopotas", "hippowdon", "sandile", "krokorok", "krookodile", "silicobra", "sandaconda", "mudbray", "mudsdale", "larvesta", "volcarona", "bagon", "shelgon", "salamence", "tinkatink", "tinkatuff", "tinkaton", "hatenna", "hattrem", "hatterene", "impidimp", "morgrem", "grimmsnarl", "wiglett", "wugtrio", "bombirdier", "finizen", "palafin", "varoom", "revavroom", "cyclizar", "orthworm", "sableye", "shuppet", "banette", "falinks", "hawlucha", "spiritomb", "noibat", "noivern", "dreepy", "drakloak", "dragapult", "glimmet", "glimmora", "rotom", "greavard", "houndstone", "oranguru", "passimian", "komala", "larvitar", "pupitar", "tyranitar", "stonjourner", "eiscue", "pincurchin", "sandygast", "palossand", "slowpoke", "slowbro", "slowking", "shellos", "gastrodon", "shellder", "cloyster", "qwilfish", "qwilfish-hisuian", "overqwil", "luvdisc", "finneon", "lumineon", "bruxish", "alomomola", "skrelp", "dragalge", "clauncher", "clawitzer", "tynamo", "eelektrik", "eelektross", "mareanie", "toxapex", "flamigo", "dratini", "dragonair", "dragonite", "snom", "frosmoth", "snover", "abomasnow", "delibird", "cubchoo", "beartic", "snorunt", "glalie", "froslass", "cryogonal", "cetoddle", "cetitan", "bergmite", "avalugg", "avalugg-hisuian", "rufflet", "braviary", "braviary-hisuian", "pawniard", "bisharp", "kingambit", "deino", "zweilous", "hydreigon", "veluza", "dondozo", "tatsugiri", "great-tusk", "scream-tail", "brute-bonnet", "flutter-mane", "slither-wing", "sandy-shocks", "iron-treads", "iron-bundle", "iron-hands", "iron-jugulis", "iron-moth", "iron-thorns", "frigibax", "arctibax", "baxcalibur", "gimmighoul", "gholdengo", "wo-chien", "chien-pao", "ting-lu", "chi-yu", "roaring-moon", "iron-valiant", "koraidon", "miraidon", "walking-wake", "iron-leaves"];
+*/
+
+let fully = ["meowscarada", "skeledirge", "quaquaval", "oinkologne", "spidops", "lokix", "jumpluff", 
+"talonflame", "pawmot", "houndoom", "gumshoos", "greedent", "sunflora", "kricketune", "vivillon", 
+"vespiquen", "corviknight", "blissey", "azumarill", "masquerain", "floatzel", "quagsire", "clodsire", 
+"golduck", "drednaw", "wigglytuff", "gardevoir", "gallade", "hypno", "gengar", "maushold", "raichu", 
+"dachsbun", "slaking", "tsareena", "arboliva", "sudowoodo", "lycanroc", "lycanroc-midnight", 
+"lycanroc-dusk", "coalossal", "luxray", "staraptor", "oricorio", "ampharos", "lilligant", 
+"lilligant-hisuian", "breloom", "flapple", "appletun", "grumpig", "squawkabilly", "mismagius", "hariyama", 
+"crabominable", "salazzle", "donphan", "copperajah", "garchomp", "garganacl", "pelipper", "gyarados", 
+"barraskewda", "basculegion", "swalot", "persian", "persian-alolan", "perrserker", "drifblim", 
+"florges", "dugtrio", "dugtrio-alolan", "torkoal", "camerupt", "bronzong", "haxorus", "annihilape", 
+"medicham", "lucario", "armarouge", "ceruledge", "whiscash", "bellibolt", "goodra", "goodra-hisuian", 
+"toxicroak", "kilowattrel", "vaporeon", "jolteon", "flareon", "espeon", "umbreon", "leafeon", "glaceon", 
+"sylveon", "dudunsparce", "sawsbuck", "farigiraf", "muk", "muk-alolan", "mabosstiff", "toxtricity", 
+"dedenne", "pachirisu", "grafaiai", "wyrdeer", "amoonguss", "electrode", "electrode-hisuian", 
+"magnezone", "ditto", "arcanine", "arcanine-hisuian", "ursaluna", "zangoose", "seviper", "altaria", 
+"gogoat", "tauros", "tauros-paldean", "tauros-paldean-fire", "tauros-paldean-water", "pyroar", "skuntank", 
+"zoroark", "zoroark-hisuian", "weavile", "sneasler", "honchkrow", "gothitelle", "polteageist", "mimikyu", 
+"klefki", "indeedee", "indeedee-female", "brambleghast", "toedscruel", "tropius", "lurantis", "klawf", 
+"scovillain", "cacturne", "rabsca", "venomoth", "forretress", "kleavor", "scizor", "heracross", "espathra",
+"hippowdon", "krookodile", "sandaconda", "mudsdale", "volcarona", "salamence", "tinkaton", "hatterene", 
+"grimmsnarl", "wugtrio", "bombirdier", "palafin", "revavroom", "cyclizar", "orthworm", "sableye", "banette",
+"falinks", "hawlucha", "spiritomb", "noivern", "dragapult", "glimmora", "rotom", "houndstone", "oranguru", 
+"passimian", "komala", "tyranitar", "stonjourner", "eiscue", "pincurchin", "palossand", "slowbro",
+"slowbro-galarian", "slowking-galarian", "slowking", "gastrodon", "cloyster", "qwilfish", "overqwil", 
+"luvdisc", "lumineon", "bruxish", "alomomola", "dragalge", "clawitzer", "eelektross", "toxapex", "flamigo", 
+"dragonite", "frosmoth", "abomasnow", "delibird", "beartic", "glalie", "froslass", "cryogonal", 
+"cetitan", "avalugg", "avalugg-hisuian", "braviary", "braviary-hisuian", "kingambit", 
+"hydreigon", "veluza", "dondozo", "tatsugiri", "baxcalibur", "gholdengo", "charizard", "typhlosion",
+"typhlosion-hisuian", "samurott", "samurott-hisuian", "chesnaught", "delphox", "greninja", "carbink",
+"decidueye", "decidueye-hisuian", "rillaboom", "cinderace", "inteleon", "raichu-alolan"];
+
+
+const box = document.querySelector('.pokes')
+fully.map(function(poke){
+    let img = document.createElement('img');
+    img.src = `sprites/${poke}.png`;
+    img.alt = poke;
+    img.className = "pokemon";
+    box.append(img);
+    return;
+});
